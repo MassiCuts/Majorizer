0b49569c741c1aa363e7d49f0033a07abb18160a
MassiCuts <maxcutugno16@gmail.com>
2019-11-24 22:17:20 -0500

    Reformatted Database. Added get/save advisor/student

--- src/framework/User.java
+++ src/framework/User.java
@@ -1,24 +1,58 @@
 package framework;
 
 public abstract class User {
-	private String name;
+	private int userID;
+	private String universityID;
+	private String firstName;
+	private String lastName;
 	private String username;
 	private String password;
-	private int id;
-	private boolean userIsStudent;
 	
-	public User(String name, String username, String password, int id, boolean userIsStudent) {
-		this.name = name;
+	public User(int userID, String universityID, String firstName, String lastName, String username, String password) {
+		this.userID = userID;
+		this.universityID = universityID;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.username = username;
 		this.password = password;
-		this.id = id;
-		this.userIsStudent = userIsStudent;
 	}
 
-	public String getName() {
-		return name;
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setUniversityID(String universityID) {
+		this.universityID = universityID;
+	}
+	
+	public String getUniversityID() {
+		return universityID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
 	}
 
+	public String getLastName() {
+		return lastName;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
@@ -27,11 +61,11 @@ public abstract class User {
 		return password;
 	}
 
-	public int getId() {
-		return id;
+	public int getUserId() {
+		return userID;
 	}
 
 	public boolean isUserIsStudent() {
-		return userIsStudent;
+		return this instanceof Student;
 	}
 }
